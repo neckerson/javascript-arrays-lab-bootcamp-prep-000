99dd9c5c87b5bd9a9797eeaf31e02bd0f3a09333
Nate Eckerson <nate.eckerson@gmail.com>
2018-05-27 17:44:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much."
 
-kittens = ["Milo", "Otis", "Garfield"]
+//kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name) {
   return kittens.push(name)
