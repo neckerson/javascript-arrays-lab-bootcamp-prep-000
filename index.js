fb457c5a7f0360a373e51f97dec0e10aea0a595c
Nate Eckerson <nate.eckerson@gmail.com>
2018-05-27 17:52:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function destructivelyAppendKitten(name) {
   return kittens.push(name)
 }
 
-function destructivelyAppendKitten(name) {
-  return kittens.push(name)
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name)
 }
